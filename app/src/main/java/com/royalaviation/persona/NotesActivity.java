1e7edadbd7d57a3fdd60da9364ee6abfa78673e0
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-11-10 00:47:13 +0530

    Minor Dependency Updation

--- app/src/main/java/com/royalaviation/persona/NotesActivity.java
+++ app/src/main/java/com/royalaviation/persona/NotesActivity.java
@@ -1,16 +1,27 @@
 package com.royalaviation.persona;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class NotesActivity extends AppCompatActivity {
 
     FloatingActionButton btnActFloat;
+    RecyclerView mrecylerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
